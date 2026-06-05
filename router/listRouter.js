8ff97ce009a4c3ab103c686c39fcffc1a532a8e5
SatputePayal <satputepayal972@gmail.com>
2026-06-05 21:21:23 +0530

    feat: add new listing form and UI enhancements

--- router/listRouter.js
+++ router/listRouter.js
@@ -3,6 +3,9 @@ const express = require("express");
 const router = express.Router();
 const listController = require("../controller/list");
 
-router.get("/", listController.get_Form);
+
+router.get("/", listController.get_Listing);
+
+router.get("/new", listController.get_Form);
 
 module.exports = router;
